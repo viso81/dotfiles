7f529011d6cea9c60a97e4bbfc610f4a41fa6f9b
viso81 <z_fun@live.cn>
2026-07-26 20:20:02 +0800

    初始化提交：fish配置 + bat-smart + zkn

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    starship init fish | source
+
+    alias ll 'eza -l --icons'
+    alias ls 'eza --icons'
+    alias lt 'eza --tree --level=2 --icons'
+
+    alias bat="bat-smart"
+    alias hx="helix"
+end
+
+# 配置zk
+set -gx ZK_NOTEBOOK_DIR "$HOME/Wiki"
+set -gx EDITOR helix
+set -gx VISUAL helix
