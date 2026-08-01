955fd8c1baf4423b5be82213fec6b92fec142fd7
viso81 <z_fun@live.cn>
2026-08-01 10:12:55 +0800

    修改qutebrowser的config.py,忽略代码检查,增加修改了一些注释

--- dot_config/private_qutebrowser/config.py
+++ dot_config/private_qutebrowser/config.py
@@ -1,10 +1,17 @@
-config.load_autoconfig()
+# pyright: reportUndefinedVariable=false
+# ruff: noqa: F821
+
 
 import dracula.draw
+
+config.load_autoconfig()
+
+# 深色主题
 dracula.draw.blood(c, {
                               "spacing" : {"vertical": 6, "horizontal": 8}
                    })
 c.colors.webpage.darkmode.enabled = True
+
 # 切换模式后关闭输入法
 config.bind('<Escape>', 'mode-leave ;; spawn fcitx5-remote -c', mode='insert')
 config.bind('o', 'set-cmd-text -s :open ;; spawn fcitx5-remote -c', mode='normal')
@@ -18,10 +25,10 @@ config.bind('<Return>', 'command-accept ;; spawn fcitx5-remote -c', mode='comman
 c.fonts.default_size = "16pt"
 # 默认语言修改为中文
 c.content.headers.accept_language = "zh-CN;q=0.9,en-US;en;q=0.8"
-
+# 开始页面和默认页面
 c.url.start_pages = ['https://www.bing.com']
 c.url.default_page = 'https://www.bing.com'
-
+# 配置搜索引擎
 c.url.searchengines = {
     'DEFAULT': 'https://www.bing.com/search?q={}',
     'g': 'https://www.google.com/search?q={}',
@@ -30,7 +37,7 @@ c.url.searchengines = {
     'du': 'https://duckduckgo.com/?q={}',
 }
 
-#c.content.proxy = 'http://127.0.0.1:7890'
+# 代理服务
 c.content.proxy = 'socks://127.0.0.1:7890'
 
 # 虚拟机下禁用硬件加速，不然会无法显示内容
